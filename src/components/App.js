622a144f13e2d5ac1e7e70bf91fba22a119cf725
harish242 <markollh242@gmail.com>
2023-06-12 10:12:38 +0530

    jkj

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,31 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 import '../styles/App.css';
 
 
 const searchArray = ["Newton", "School", "Newton School", "React", "Preact", "Node", "Mongoose", "Angular", "Vue", "Apple", "Microsoft", "Netflix", "Meta"]
 const App = () => {
-
+const [arr,setArray]=useState(searchArray)
+const [data,setData]=useState([])
+  const HandleClick=(e)=>{
+    const value=e.target.value;
+    const mass=arr.filter(a=>a.toLowerCase().includes(value.toLowerCase()))
+    setData(mass)
+    if(e.target.value==''){
+      setData([])
+    }
+  }
   
+  console.log(data)
   return (
     <div id="main">
-      
+      <input id='search-input'  onChange={HandleClick} type='text'/><br/>
+      <label>Result</label>
+      <ul>
+      {data.map(a=>{
+        return <li>{a}</li>
+      })} 
+      </ul>          
     </div>
   )
 }
